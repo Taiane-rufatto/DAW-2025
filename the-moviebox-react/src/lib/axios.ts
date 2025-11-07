1284e9ce103a8f95b44a44bbf024bfe77b670a38
Taiane Rufatto <thayrufatto84@gmail.com>
2025-11-06 21:57:03 -0300

    feat4: aula 06/11, movie card, movie component, axios

---
+++ the-moviebox-react/src/lib/axios.ts
@@ -0,0 +1,9 @@
+import axios from "axios";  
+
+export const api = axios.create({
+    baseURL: 'https://my.api.mockaroo.com',
+    headers: {
+        'Content-Type': 'application/json',
+    }
+
+}); 
